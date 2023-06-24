04995ab3f6e87f79325f67cbf7d5b4014a816c02
Ahmet Ülkü <ahmetulku98@gmail.com>
2023-06-24 17:45:10 +0300

    logout, signup functions added

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -44,12 +44,42 @@ def login_request(request):
             return redirect('djangoapp:index')
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+
+
+def logout_request(request):
+    logout(request)
+    return redirect('djangoapp:index')
+
 
 # Create a `registration_request` view to handle sign up request
-# def registration_request(request):
-# ...
+def registration_request(request):
+    context = {}
+
+    if request.method == "GET":
+        return render(request, 'djangoapp/registration.html')
+    elif request.method == "POST":
+        # Check if user exists
+        username = request.POST['username']
+        password = request.POST['password']
+        first_name = request.POST['fname']
+        last_name = request.POST['lname']
+        user_exist = False
+
+        try:
+            User.objects.get(username=username)
+            user_exist = True
+        except:
+            logger.error("New user")
+
+        if not user_exist:
+            user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,
+                                            password=password)
+            login(request, user)
+            return redirect('djangoapp:index')
+        else:
+            context['message'] = "User already exists."
+            return render(request, 'djangoapp/registration.html', context)
+
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 
